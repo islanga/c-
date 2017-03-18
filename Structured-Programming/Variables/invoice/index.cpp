52ff29805638f2c3f7abfc47c1e88140239a0dd0
Preetha Behary <Preetha.Behary@rewardsco.co.za>
2017-03-18 09:34:33 +0200

    commit changes

--- Structured-Programming/Variables/invoice/index.cpp
+++ Structured-Programming/Variables/invoice/index.cpp
@@ -6,17 +6,21 @@ int main()
 {
 	// Declarations
 	int iNo;
-	double dUnitPr, dPriceExTax, dCustPrice, dTax;
-	const double dTaxPerc = 25.0;
+	double dUnitPr, dPriceExTax, dCustPrice, dTax, bfTax;
+	double dTaxPerc = 0.0;
 
 	// Entry of quantity and unit price
-	cout << "Specify quantity and unit price: ";
+	cout << "Specify quantity and unit price and tax percent : ";
 
 	// Assign the input to the following variables
-	cin >> iNo >> dUnitPr;
+	cin >> iNo >> dUnitPr >> dTaxPerc;
 
 	// Calculations. First the price without tax
 	dPriceExTax = dUnitPr * iNo;
+
+	// Deduct 10% before tax
+	bfTax = (dPriceExTax * 10.0 / 100);
+	dPriceExTax = dPriceExTax - bfTax;
 
 	// then the tax amount
 	dTax = dPriceExTax * dTaxPerc / 100;
@@ -32,4 +36,5 @@ int main()
 	cout << "Price per unit:" << setw(8) << dUnitPr << endl;
 	cout << "Total price: " << setw(8) << dCustPrice << endl;
 	cout << "Tax: " << setw(8) << dTax << endl;
+	cout << "Total Discount : " << setw(5) << bfTax + dTax << endl;
 }
